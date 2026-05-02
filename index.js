10f95d2a525947cec4c30e59eff58587209afb04
aleariel <aarodriguez@gmail.com>
2026-05-01 19:19:35 -0600

    Update index.js

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const MIME = {
 
 const httpServer = http.createServer((req, res) => {
   let filePath = req.url === '/' ? '/client.html' : req.url;
-  filePath = path.join(__dirname, '..', filePath);
+  filePath = path.join(__dirname, filePath);
   const ext = path.extname(filePath);
   fs.readFile(filePath, (err, data) => {
     if (err) { res.writeHead(404); res.end('Not found'); return; }
